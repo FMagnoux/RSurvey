dd59759a79725cdf0af0b883992ceaaa9dc32330
FMAGNOUX <magnoux.florent@gmail.com>
2016-06-09 11:17:55 +0200

    creation classe role
    creation classe role

---
+++ Model/Role.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: FlorentMagnoux
+ * Date: 09/06/2016
+ * Time: 11:15
+ */
+class Role
+{
+    private $iRoleId;
+    private $sRoleLibel;
+
+    /**
+     * @return mixed
+     */
+    public function getIRoleId()
+    {
+        return $this->iRoleId;
+    }
+
+    /**
+     * @param mixed $iRoleId
+     * @return $this
+     */
+    public function setIRoleId($iRoleId)
+    {
+        $this->iRoleId = $iRoleId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSRoleLibel()
+    {
+        return $this->sRoleLibel;
+    }
+
+    /**
+     * @param mixed $sRoleLibel
+     * @return $this
+     */
+    public function setSRoleLibel($sRoleLibel)
+    {
+        $this->sRoleLibel = $sRoleLibel;
+        return $this;
+    }
+
+
+
+}
