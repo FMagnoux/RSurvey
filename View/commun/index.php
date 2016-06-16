92e89e05b58a1b060f92783cc763bffda707e799
rfrancois <f.romain2009@gmail.com>
2016-06-16 09:34:13 +0200

    Correction dans le header pour que la base soit /RSurvey/ et pas juste /

--- View/commun/index.php
+++ View/commun/index.php
@@ -1,34 +1,4 @@
-<!-- Always shows a header, even in smaller screens. -->
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <div class="mdl-layout__header mdl-shadow--12dp">
-        <div class="mdl-layout__header-row mdl-color--white">
-            <!-- Title -->
-            <img class="imgLogo" src="ressources/media/img/logov1.svg" alt="Logo">
-            <span class="mdl-layout-title mdl-color-text--grey-600 mdl-typography--font-bold">RSurvey</span>
-            <!-- Add spacer, to align navigation to the right -->
-            <div class="mdl-layout-spacer"></div>
-            <!-- Navigation. We hide it in small screens. -->
-            <nav class="mdl-navigation mdl-layout--large-screen-only">
-                <a id="newSurvey" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Ecrire un sondage</a>
-                <a id="login" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Se Connecter</a>
-                <a id="signup" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Créer un compte</a>
-                <a class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="#contact">Contact</a>
-            </nav>
-            <label class="mdl-button mdl-js-button mdl-button--icon">
-                <i class="material-icons mdl-color-text--grey-600 mdl-typography--font-bold">language</i>
-            </label>
-        </div>
-    </div>
-    <div class="mdl-layout__drawer">
-        <span class="mdl-layout-title">Menu</span>
-        <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="">Ecrire un sondage</a>
-            <a class="mdl-navigation__link" href="">Se connecter</a>
-            <a class="mdl-navigation__link" href="">Contact</a>
-        </nav>
-    </div>
-    <div class="mdl-layout__content">
-        <div class="page-content">
+
             <div class="homepage-landing-section mdl-typography--text-center">
                 <div class="logo-font homepage-slogan">Thé ou Café ?</div>
                 <div class="logo-font homepage-sub-slogan">R Survey vous permet de créer des sondages et d'avoir les résultats région par région.</div>
@@ -84,16 +54,3 @@
 
                 <div class="mdl-cell mdl-cell--2-col"></div>
             </div>
-        </div>
-        <div class="mdl-mini-footer">
-            <div class="mdl-mini-footer__left-section">
-                <div class="mdl-logo">Rsurvey</div>
-                <ul class="mdl-mini-footer__link-list">
-                    <li><a href="#">Aide</a></li>
-                    <li><a href="#">Mentions légales</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-</div>
