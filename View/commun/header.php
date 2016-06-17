53c77d020f7fb84966c84bd92df42b8e8ae88bf5
FMAGNOUX <magnoux.florent@gmail.com>
2016-06-17 14:04:14 +0200

    correction header et session

--- View/commun/header.php
+++ View/commun/header.php
@@ -1,64 +1,75 @@
 <!doctype html>
-   <html lang="fr">
-   <head>
-      <base href="/RSurvey/">
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <meta name="description" content="">
-      <link rel="stylesheet" href="ressources/css/fonts.css">
-      <link rel="stylesheet" href="ressources/css/material.min.css">
-      <link rel="stylesheet" href="ressources/css/jquery-modal-mdl.css">
-      <link rel="stylesheet" href="ressources/css/main.css">
-      <?php $this->insertFiles($aCss, $aCssFileNames, "<link rel=\"stylesheet\" href=\"./ressources/css/", ".css\">"); ?>
+<html lang="fr">
+<head>
+   <base href="/RSurvey/">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+   <meta name="description" content="">
+   <link rel="stylesheet" href="ressources/css/fonts.css">
+   <link rel="stylesheet" href="ressources/css/material.min.css">
+   <link rel="stylesheet" href="ressources/css/jquery-modal-mdl.css">
+   <link rel="stylesheet" href="ressources/css/main.css">
+   <?php $this->insertFiles($aCss, $aCssFileNames, "<link rel=\"stylesheet\" href=\"./ressources/css/", ".css\">"); ?>
 
-      <link rel="shortcut icon" type="image/x-icon" href="ressources/media/img/logov1.ico" />
-      <link rel="icon" type="image/x-icon" href="ressources/media/img/logov1.ico" />
-      <title>R Survey</title>
-   </head>
-   <body>
+   <link rel="shortcut icon" type="image/x-icon" href="ressources/media/img/logov1.ico" />
+   <link rel="icon" type="image/x-icon" href="ressources/media/img/logov1.ico" />
+   <title>R Survey</title>
+</head>
+<body>
 
-   <!-- Always shows a header, even in smaller screens. -->
-   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <div class="mdl-layout__header mdl-shadow--12dp">
-         <div class="mdl-layout__header-row mdl-color--white">
-            <!-- Title -->
-            <img class="imgLogo" src="ressources/media/img/logov1.svg" alt="Logo">
-            <span class="mdl-layout-title mdl-color-text--grey-600 mdl-typography--font-bold">RSurvey</span>
-            <!-- Add spacer, to align navigation to the right -->
-            <div class="mdl-layout-spacer"></div>
-            <!-- Navigation. We hide it in small screens. -->
-            <nav class="mdl-navigation mdl-layout--large-screen-only">
-               <a id="newSurvey" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Ecrire un sondage</a>
-               <?php
-               if(isset($_SESSION['iIdUser']) || !empty($_SESSION['iIdUser']) ) {
-                  ?>
-                  <a id="disconnect" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold"
-                     href="">Se Déconnecter</a>
-                  <?php
-               }
-               else {
-                  ?>
-                  <a id="login" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Se
-                     Connecter</a>
-                  <?php
-               }
+<!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+   <div class="mdl-layout__header mdl-shadow--12dp">
+      <div class="mdl-layout__header-row mdl-color--white">
+         <!-- Title -->
+         <img class="imgLogo" src="ressources/media/img/logov1.svg" alt="Logo">
+         <span class="mdl-layout-title mdl-color-text--grey-600 mdl-typography--font-bold">RSurvey</span>
+         <!-- Add spacer, to align navigation to the right -->
+         <div class="mdl-layout-spacer"></div>
+         <!-- Navigation. We hide it in small screens. -->
+         <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <a id="newSurvey" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Ecrire un sondage</a>
+            <?php
+            if(isset($_SESSION['iIdUser']) || !empty($_SESSION['iIdUser']) ) {
                ?>
-               <a id="signup" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Créer un compte</a>
-               <a class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="#contact">Contact</a>
-            </nav>
-            <label class="mdl-button mdl-js-button mdl-button--icon">
-               <i class="material-icons mdl-color-text--grey-600 mdl-typography--font-bold">language</i>
-            </label>
-         </div>
-      </div>
-      <div class="mdl-layout__drawer">
-         <span class="mdl-layout-title">Menu</span>
-         <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="">Ecrire un sondage</a>
-            <a class="mdl-navigation__link" href="">Se connecter</a>
-            <a class="mdl-navigation__link" href="">Contact</a>
+               <a id="disconnect" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold"
+                  href="disconnect.html">Se Déconnecter</a>
+               <?php
+            }
+            else {
+               ?>
+               <a id="login" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Se Connecter</a>
+                  <a id="signup" class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="">Créer un compte</a>
+
+               <?php
+            }
+            ?>
+            <a class="mdl-navigation__link mdl-color-text--grey-600 mdl-typography--font-bold" href="#contact">Contact</a>
          </nav>
+         <label class="mdl-button mdl-js-button mdl-button--icon">
+            <i class="material-icons mdl-color-text--grey-600 mdl-typography--font-bold">language</i>
+         </label>
       </div>
-      <div class="mdl-layout__content">
-         <div class="page-content">
+   </div>
+   <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Menu</span>
+      <nav class="mdl-navigation">
+         <a class="mdl-navigation__link" href="">Ecrire un sondage</a>
+         <?php
+         if(isset($_SESSION['iIdUser']) || !empty($_SESSION['iIdUser']) ) {
+            ?>
+            <a id="disconnect" class="mdl-navigation__link" href="disconnect.html">Se déconnecter</a>
+
+            <?php
+         } else {
+            ?>
+            <a id="login" class="mdl-navigation__link" href="">Se connecter</a>
+            <?php
+         }
+         ?>
+         <a class="mdl-navigation__link" href="">Contact</a>
+      </nav>
+   </div>
+   <div class="mdl-layout__content">
+      <div class="page-content">
