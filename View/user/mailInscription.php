e1ce1792b451123acf2e027db4c5c1bfa783730a
rfrancois <f.romain2009@gmail.com>
2016-06-17 16:55:19 +0200

    Mail un peu plus jolie

--- View/user/mailInscription.php
+++ View/user/mailInscription.php
@@ -2,4 +2,13 @@
 $aMail["subject"] = "Activation du compte";
 $aMail["fromName"] = "R Survey";
 $aMail["from"] = "no-reply@r-survey.com";
-$aMail["message"] = "<p><a href='http://r-survey.com/confirmer-compte/".$sId."/".$sToken."'>Cliquez ici</a> pour activer votre compte.</p>";
+$aMail["message"] = "
+<html>
+  <head>
+   <title>".$aMail["subject"]."</title>
+  </head>
+  <body>
+    <p><a href='http://r-survey.com/confirmer-compte/".$sId."/".$sToken."'>Cliquez ici</a> pour activer votre compte.</p>
+  </body>
+</html>
+";
