9c6085695681a1d139deace75bc12a67bd81a6b6
Romain Geoffrey François <f.romain2009@gmail.com>
2016-06-22 20:53:33 +0200

    Redirection 404

--- View/commun/header.php
+++ View/commun/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="fr">
 <head>
-   <base href="/RSurvey/">
+   <base href="/projetweb/RSurvey/">
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
