7f3b1d588037a147286ba08d6aa9018fdb549724
rfrancois <f.romain2009@gmail.com>
2016-06-22 11:42:39 +0200

    Changement des balises title et description en fonction de la page

--- View/admin/header.php
+++ View/admin/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="description" content="">
+    <meta name="description" content="<?= isset($aMetaDescription[$this->page]) ? $aMetaDescription[$this->page] : "R Survey" ?>">
     <link rel="stylesheet" href="ressources/css/fonts.css">
     <link rel="stylesheet" href="ressources/css/material.min.css">
     <link rel="stylesheet" href="ressources/css/jquery-modal-mdl.css">
@@ -14,7 +14,7 @@
 
     <link rel="shortcut icon" type="image/x-icon" href="ressources/media/img/logov1.ico" />
     <link rel="icon" type="image/x-icon" href="ressources/media/img/logov1.ico" />
-    <title>R Survey</title>
+    <title><?= isset($aMetaTitles[$this->page]) ? $aMetaTitles[$this->page] : "R Survey" ?></title>
 </head>
 <body>
 
