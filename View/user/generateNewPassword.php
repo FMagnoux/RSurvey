453e1305c70012cc1dfdaca37a2419cb04d6168c
Romain Geoffrey François <f.romain2009@gmail.com>
2016-06-22 21:40:22 +0200

    Petite correction mail

--- View/user/generateNewPassword.php
+++ View/user/generateNewPassword.php
@@ -1,7 +1,7 @@
 <?php
-if(!empty($success)) echo "<p class=\"mdl-typography--text-center\"" . $success . "</p>";
+if(!empty($success)) echo "<p class=\"mdl-typography--text-center\">" . $success . "</p>";
 else {
-    if(!empty($error)) echo "<p class=\"mdl-typography--text-center\"" . $error . "</p>"; ?>
+    if(!empty($error)) echo "<p class=\"mdl-typography--text-center\">" . $error . "</p>"; ?>
         <form method="post">
             <input type="password" name="sUsrPassword">
             <input type="password" name="sUsrConfirmPassword">
